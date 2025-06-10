0c14ae3b1ccb75063961ab564bf570e0cfed0f91
Carlos Villa <xxcarlos489xx@gmail.com>
2025-06-10 17:36:32 -0500

    generate sota and references IEEE

--- server/api/topics/generate-sota.post.ts
+++ server/api/topics/generate-sota.post.ts
@@ -4,6 +4,7 @@ import { generateSotaSchema } from "~/server/libs/validation/topic";
 import { TopicService } from "~/server/services/topic.service";
 import { spawn } from "child_process";
 import { join } from "path";
+import fs from 'node:fs';
 
 const topicService = new TopicService();
 
@@ -15,12 +16,17 @@ export default defineEventHandler(async (event) => {
     const { topicId } = body;
     const topic = await topicService.getTopic(topicId);
 
-    if (!topic?.id) {
+    if (!topic?.id || !topic.titulo || !topic.slug) {
         throw createError({
-        statusCode: 400,
-        message: "Topic no existe",
+            statusCode: 400,
+            message: "Topic no existe o le faltan datos (nombre, slug)",
         });
     }
+
+    const USER_ID       =   '1';
+    const slug          =   `${topic.slug}_${USER_ID}`;
+    const storageDir    =   join(process.cwd(), 'server', 'storage', slug);
+    fs.mkdirSync(storageDir, { recursive: true });
 
     const pythonPath  = join(process.cwd(), 'scripts', 'Scripts', 'python.exe')
     const scriptPath  = join(process.cwd(), 'scripts', 'consulta_vector_db.py')
@@ -30,7 +36,8 @@ export default defineEventHandler(async (event) => {
     const scriptArgs = [
         scriptPath,
         idTopic,
-        tema
+        tema,
+        storageDir
     ];
 
     // Ejecutar python en segundo plano (sin bloquear)
@@ -41,5 +48,9 @@ export default defineEventHandler(async (event) => {
     
     pyProcess.unref()
 
-    return { success: true, topic };
+    return {    statusCode: 200,
+                success:true,
+                message:"El sota estara disponible en unos momentos."
+                // topic
+            };
 });
